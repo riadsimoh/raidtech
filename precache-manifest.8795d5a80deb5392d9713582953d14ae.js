8a42d1e1c4cab2d31a150b7ac5cbb9664a713ebb
Riadh SI MOH <simoh.riadh@gmail.com>
2020-10-11 17:41:40 +0200

    Updates

---
+++ precache-manifest.8795d5a80deb5392d9713582953d14ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b0cd5a698134ba2024f0cef9a70634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30887a1b351b0243caba",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4915733972b7b43164b1",
+    "url": "/static/css/main.82604511.chunk.css"
+  },
+  {
+    "revision": "30887a1b351b0243caba",
+    "url": "/static/js/2.f121568b.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/static/js/2.f121568b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4915733972b7b43164b1",
+    "url": "/static/js/main.d78accbd.chunk.js"
+  },
+  {
+    "revision": "dd8c9df39ffea6af3075",
+    "url": "/static/js/runtime-main.80ee6a9a.js"
+  },
+  {
+    "revision": "64d617929291a2674b23877abacdc90e",
+    "url": "/static/media/nature.64d61792.jpg"
+  }
+]);
